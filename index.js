d7fbfa77c03d725f34805898150c082266f17451
Mario Choi <choi@Marioui-iMac.local>
2024-12-22 15:54:16 +0900

    Restore Supabase integration and JS scripts

---
+++ index.js
@@ -0,0 +1,27 @@
+var map = new kakao.maps.Map(document.getElementById('map'), {
+  center: new kakao.maps.LatLng(37.582004, 126.984770),
+  level: 4
+});
+getCount().then(({count}) => {
+  if (count <= MAX_RECENT_COMMENTS) {
+    document.getElementById('guestbook-button').style.display = 'none';
+  }
+});
+renderGuestbook(getDataRecent);
+document.getElementById('submit-form-button').addEventListener('click', () => {
+  submitForm();
+});
+
+var coll = document.getElementsByName('accounts');
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
